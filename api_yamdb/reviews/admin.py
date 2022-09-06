e2df5be1f35a6d3c7ed88662e6b8e08abaa420bf
Василиса Немоляева <mtk74@yandex.ru>
2022-09-06 23:02:47 +0500

    fix: PEP8

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Genre, Review, Title, Comment
+from reviews.models import Category, Genre, Review, Title, Comment
 
 
 class CategoryAdmin(admin.ModelAdmin):
